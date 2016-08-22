a272a954d8df0dff7c19538994928e7efc8fad95
Renan Moreira Santos <renanmoreirasan@gmail.com>
2016-08-22 20:20:46 -0300

    Publica primeira versão produtiva

--- AppAluno/app/src/main/java/com/example/renan/appaluno/MainActivity.java
+++ AppAluno/app/src/main/java/com/example/renan/appaluno/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.renan.appaluno;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import model.Model;
+import object.Student;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,12 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        try {
+            String error = intent.getStringExtra("error");
+            TextView textview = (TextView) findViewById(R.id.textView2);
+            textview.setText(error);
+        } catch (Exception e) {
+
+        }
     }
 
-    public void searchStudentByRA(View view){
+    public void searchStudentByRA(View view) {
         EditText editRa = (EditText) findViewById(R.id.editText);
-        String ra = String.valueOf(editRa.getText());
-        TextView result = (TextView) findViewById(R.id.textView2);
-        result.setText(ra);
+        Intent intent = new Intent(this, StudentDetails.class);
+        intent.putExtra("ra", String.valueOf(editRa.getText()));
+        startActivity(intent);
     }
 }
