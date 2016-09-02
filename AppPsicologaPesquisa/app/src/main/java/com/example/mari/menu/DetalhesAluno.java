609e59014b4d47d301b5f3c7eef16adc99361766
Helen Reis <helen1705@hotmail.com>
2016-09-02 19:15:00 -0300

    Novo Detalhes Aluno

--- AppPsicologaPesquisa/app/src/main/java/com/example/mari/menu/DetalhesAluno.java
+++ AppPsicologaPesquisa/app/src/main/java/com/example/mari/menu/DetalhesAluno.java
@@ -2,30 +2,25 @@ package com.example.mari.menu;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.RadarChart;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.RadarData;
-import com.github.mikephil.charting.data.RadarDataSet;
-
-import java.util.ArrayList;
 
 import model.Model;
-import object.Competencie;
 import object.Student;
 
-/**
- * Created by Mari on 17/08/2016.
- */
 public class DetalhesAluno extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    Student student = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +37,7 @@ public class DetalhesAluno extends AppCompatActivity {
 
         Model model = new Model();
 
-        Student student = model.searchByCode(ra);
+        student = model.searchByCode(ra);
 
         if (student == null) {
             Intent main_intent = new Intent(this, PesquisarAluno.class);
@@ -50,39 +45,23 @@ public class DetalhesAluno extends AppCompatActivity {
             startActivity(main_intent);
         }
 
-        RadarChart bar = (RadarChart) findViewById(R.id.chart);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
 
-        ArrayList<Entry> entries = new ArrayList<>();
-        int index = 0;
-        for (Competencie competencie : student.getCompetencies()) {
-            entries.add(new Entry(competencie.getWeight(), index));
-            index++;
-        }
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
 
-        RadarDataSet dataset = new RadarDataSet(entries, "# of Calls");
-
-        ArrayList<String> labels = new ArrayList<String>();
-        for (Competencie competencie : student.getCompetencies()) {
-            labels.add(competencie.getType());
-        }
-
-        dataset.setLineWidth(3);
-        dataset.setDrawFilled(true);
-
-        RadarData data = new RadarData(labels, dataset);
-        bar.setData(data);
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
 
+    private void setupViewPager(ViewPager viewPager) {
+        GraficoTab grafico = new GraficoTab();
+        grafico.passData(student);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new OneFragment(), "Detalhes");
+        adapter.addFragment(grafico, "Gráfico");
+        viewPager.setAdapter(adapter);
+    }
 
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -94,9 +73,6 @@ public class DetalhesAluno extends AppCompatActivity {
         if (id == android.R.id.home) {
             finish();
         }
-
         return true;
     }
-
-
 }
---
+++ AppPsicologaPesquisa/app/src/main/java/com/example/mari/menu/TwoFragment.java
@@ -0,0 +1,31 @@
+package com.example.mari.menu;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by helen on 24/08/2016.
+ */
+public class TwoFragment extends Fragment {
+
+
+    public TwoFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+}
