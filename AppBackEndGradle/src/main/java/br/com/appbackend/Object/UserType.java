50975ad6d18ffed552882a684942e9675bb19228
perlamber <cris_2008_@hotmail.com>
2016-10-05 20:21:27 -0300

    v2 de login de usuario, v6 do envio de comentario para o aluno

---
+++ AppBackEndGradle/src/main/java/br/com/appbackend/Object/UserType.java
@@ -0,0 +1,13 @@
+package br.com.appbackend.Object;
+
+public enum UserType {
+	
+	STUDENT("Student"), PSYCHOLOGIST("Psychologist"), OTHER("");
+	
+	public String type;
+	
+	private UserType(String type)
+	{
+		this.type = type;
+	}
+}
