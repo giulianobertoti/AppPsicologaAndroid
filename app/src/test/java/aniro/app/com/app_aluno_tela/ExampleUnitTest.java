3f1a447f7ced450a2c96419cd21d741cab376049
Roberto Pereira <rxpjunior@gmail.com>
2016-10-19 18:36:21 -0300

    Criacao APPaluno e sua estrutura

---
+++ app/src/test/java/aniro/app/com/app_aluno_tela/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package aniro.app.com.app_aluno_tela;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
